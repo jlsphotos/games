f71b31a472f831b07115df72c35ba6ff0af011a1
jlsphotos <admin@jlsphotos.co.uk>
2017-07-12 23:42:38 +0100

    l

---
+++ lists.py
@@ -0,0 +1,67 @@
+import random
+import time
+
+
+# version 0.1 in.
+
+print("Spining Wheel")
+time.sleep(random.randint(1,3))
+reds = (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36)
+blacks = (2, 4, 6, 8, 10, 11,13, 15, 17, 20, 22, 24,26, 28, 29, 31, 33, 35)
+st1 = range(1, 12)
+st2 = range(1, 12)
+
+
+def spin_the_wheel():
+    spin = random.randint(0,36)
+    return spin
+
+
+def check_colour(n):
+    if n in reds:
+        return "Red"
+    elif n in blacks:
+        return "Black"
+    else:
+        return "Green"
+
+
+def check_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+def check_dozen(n):
+    if n in range(1,12):
+        return "1st Dozen"
+    elif n in range(13,24):
+        return "2nd Dozen"
+    elif n in range(25,36):
+        return "3rd Dozen"
+    else:
+        return "zero"
+def high_low(n):
+    if n > 18:
+        return "High"
+    else:
+        return "Low"
+
+result = spin_the_wheel()
+colour = check_colour(result)
+oddness = check_odd(result)
+dozen = check_dozen(result)
+highlow = high_low(result)
+
+
+print("The result is %s %s. \nThe number is %s.\nis the %s\nAnd is %s " % (result, colour, oddness,dozen,highlow))
+
+
+
+
+
+
+
+
+
