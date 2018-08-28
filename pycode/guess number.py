13c275d58e55c0876647cdd926e00d283c740d5e
jlsphotos <admin@jlsphotos.co.uk>
2018-08-28 22:56:31 +0100

    l

--- pycode/guess number.py
+++ pycode/guess number.py
@@ -1,3 +1,34 @@
-print("----------------------------------")
-print("        Guess Number")
-print("----------------------------------")
+import random
+
+num = random.randint(1, 100)
+counter = 0
+guess = -1
+
+
+def header():
+    print("----------------------------------")
+    print("        Guess Number")
+    print("----------------------------------")
+    print()
+
+def how_close(g, n):
+    num = abs(g - n)
+    if (num > 5) and (num < 10):
+        print('Getting close!!!!!!')
+    elif num < 5:
+        print('Almost on it')
+    else:
+        pass
+
+header()
+
+while guess != num:
+    guess = int(input('have a guess: '))
+    how_close(guess, num)
+    counter += 1
+    if guess < num:
+        print('Too low!')
+    elif guess > num:
+        print('Too High')
+    else:
+        print('bingo, you took {} attempts to guess the number {}'.format(counter,num))
