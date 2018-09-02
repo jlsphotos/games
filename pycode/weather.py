a2b2ff65fa37e7087c9a58f10572fbc751b4e07b
jlsphotos <admin@jlsphotos.co.uk>
2018-09-03 00:28:26 +0100

    weather app

--- pycode/weather.py
+++ pycode/weather.py
@@ -1,1 +1,14 @@
-import
+from bs4 import BeautifulSoup as bs
+import requests as r
+import pandas as pd
+
+# variables
+table = pd.read_table('https://www.wunderground.com/history/daily/gb/castleford/IWESTYOR64/date/2018-8-27')
+
+print(table.head())
+url = 'https://www.wunderground.com/hourly/gb/'
+hist = 'history/daily/gb/castleford/IWESTYOR64/date/2018-8-27'
+location = 'castleford'
+full_url = url + location
+
+print(full_url)
